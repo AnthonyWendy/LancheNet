868946dc4056e3c69c39c5020d24d6fcb6801fda
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2023-09-06 20:11:22 -0300

    initial_commit

---
+++ src/main/java/com/lanchenet/LancheNet/LancheNetApplication.java
@@ -0,0 +1,13 @@
+package com.lanchenet.LancheNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancheNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancheNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanchenet/LancheNet/LancheNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanchenet.LancheNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancheNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
